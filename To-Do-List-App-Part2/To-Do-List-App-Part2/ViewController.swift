c0bbfbff83a9914e5449d5110fb8abc4ed27bdbb
Abdeali-53 <amody1@my.centennialcollege.ca>
2020-12-02 18:06:20 -0500

    Created UI

---
+++ To-Do-List-App-Part2/To-Do-List-App-Part2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do-List-App-Part2
+//
+//  Created by Abdeali Mody on 2020-12-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
